7cfb54e69fb0de7828225add825e6cbe858ea255
Wen-Tien Chang <ihower@gmail.com>
2015-12-03 10:32:48 +0800

    new higher-order-fun-pre-post-processing example

--- higher-order-fun-pre-post-processing.js
+++ higher-order-fun-pre-post-processing.js
@@ -1,11 +1,42 @@
-function logging(your_proceduce) {
-  console.log("Entering");
+// npm install sleep
+var sleep = require('sleep');
 
-  your_proceduce();
+// before: we insert benchmark code inside function
+function foo1(){
+  start_time = new Date();
 
-  console.log("Leaving");
+  sleep.sleep(1)
+
+  end_time = new Date();
+  console.log( end_time - start_time )
 }
 
-logging(function(){
-  console.log("your code here");
+function bar1(){
+  start_time = new Date();
+
+  sleep.sleep(2)
+
+  end_time = new Date();
+  console.log( end_time - start_time )
+}
+
+foo1()
+bar1()
+
+// after: we extract benchmark code
+function benchmark(fun) {
+  start_time = new Date();
+  fun()
+  end_time = new Date();
+  console.log( end_time - start_time )
+}
+
+function foo2(){
+  sleep.sleep(1)
+}
+
+benchmark(foo2)
+
+benchmark(function(){
+  sleep.sleep(2)
 })
